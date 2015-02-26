61f91a3261336f121c048c8447c137dfb16a4bd6
Doron Tal <doron@tracktunes.org>
2015-02-26 17:27:27 -0500

    js3 mode back in

--- lib/emacs/autoinclude/header.m
+++ lib/emacs/autoinclude/header.m
@@ -1,5 +1,8 @@
 function OUT = header(IN)
 
 % function OUT = header(IN)
-% Author:
-% Date created:
+%
+% Author: Doron Tal (dtal@email.arc.nasa.gov)
+% Date created: Feb, 2005
+%
+% $Revision: 1.3 $ $Date: 2005/02/05 21:10:28 $
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# sets up a new user while backing up skeleton replaced files
+
+MY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DATE="`date +%F`"
+
+if [ ! -e $HOME/BACKUPS ]; then
+    mkdir $HOME/BACKUPS
+fi
+
+if [ ! -e $HOME/.DEL ]; then
+    mkdir $HOME/.DEL
+fi
+
+cd $HOME
+backups=""
+for i in .[a-z,A-Z]*; do
+    if [ -e $MY_DIR/$i ]; then
+        backups="$i $backups"
+    fi
+done
+echo $backups
+
+# backup
+tar czvhf $HOME/BACKUPS/dot_files$DATE.tgz $backups
+
+# destructive
+for i in .[a-z,A-Z]*; do
+    if [ -e $MY_DIR/$i ]; then
+        mv -f $MY_DIR/$i $HOME
+    fi
+done
+
+cp $MY_DIR/bin $HOME
+cp $MY_DIR/lib $HOME
